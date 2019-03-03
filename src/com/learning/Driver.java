186729f6d1e25ab5514ca1704dfea4ba5045b4e7
Abdi Kedie <abdikadir.kedie@jonahgroup.com>
2019-03-02 22:24:41 -0500

    First commit

---
+++ src/com/learning/Driver.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=10; i++) {
+			System.out.println("Jenkins ---- "+i);
+		}
+
+	}
+
+}
